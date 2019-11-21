2792940c794b931bbe27bab634dd3f435415a8b7
ChrisYu <344190767@qq.com>
2019-11-21 23:57:54 +0800

    1. 自定义View

--- app/src/main/java/com/yusq/videodemo/model/hangzhou/adapter/DesignViewFragment.java
+++ app/src/main/java/com/yusq/videodemo/model/hangzhou/adapter/DesignViewFragment.java
@@ -1,17 +1,48 @@
 package com.yusq.videodemo.model.hangzhou.adapter;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.yusq.videodemo.R;
 import com.yusq.videodemo.base.BaseFragment;
 import com.yusq.videodemo.inject.SetViewInject;
+import com.yusq.videodemo.view.LikeClickView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 @SetViewInject(mainLayoutId = R.layout.design_view_fragment_layout)
 public class DesignViewFragment extends BaseFragment {
 
+    @BindView(R.id.design_view_is_like)
+    LikeClickView designViewIsLike;
+    Unbinder unbinder;
+
     @Override
     public void afterBindView() {
-
+          designViewIsLike.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View view) {
+                  designViewIsLike.ChangeIsLike();
+              }
+          });
     }
 
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = super.onCreateView(inflater, container, savedInstanceState);
+        unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
 }
--- app/src/main/java/com/yusq/videodemo/view/LikeClickView.java
+++ app/src/main/java/com/yusq/videodemo/view/LikeClickView.java
@@ -1,22 +1,66 @@
 package com.yusq.videodemo.view;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.yusq.videodemo.R;
+
 public class LikeClickView extends View {
+
+    private  boolean isLike;
+    private Bitmap iBitmap;
+    private Bitmap backBitmap;
+    private Paint paint;
 
     public LikeClickView(Context context) {
         super(context);
+        init();
     }
 
     public LikeClickView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public LikeClickView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.LikeClickView);
+        isLike = typedArray.getBoolean(R.styleable.LikeClickView_is_like , false);
+        typedArray.recycle();
+        init();
     }
 
+    private void init() {
+        paint = new Paint();
+        Resources resources = getResources();
+        iBitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher);
+        backBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_midea_yjkm_baner);
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if(iBitmap != null && paint != null) {
+            if(isLike) {
+                canvas.drawBitmap(iBitmap, 100, 200, paint);
+            }else {
+                canvas.drawBitmap(backBitmap, 100, 200, paint);
+            }
+        }
+    }
+
+
+    public void ChangeIsLike() {
+        isLike = !isLike;
+        invalidate();
+    }
 }
